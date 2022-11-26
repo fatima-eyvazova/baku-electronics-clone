45da9513a3f274537a93bd2e285bc12fb5bdec91
Fatima Eyvazova <fatima.eyvazovaa@gmail.com>
2022-11-26 13:12:17 +0400

    final commit

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,5 @@
-import { Header } from "../header";
-import { Footer } from "../footer";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
 const Layout = ({ children }) => {
   return (
